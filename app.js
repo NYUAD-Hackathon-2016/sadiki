24f85679631aea9fe10ecad21bee8fb1afeee6ea
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2016-04-16 10:13:14 +0400

    Mongoose dependency.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var sendTextMessage = require('./send-text-message')
+var mongoose = require('mongoose');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -12,33 +13,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Process application/json
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://localhost/test');
+
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot');
+  res.send('Hello world, I am a chat bot');
 });
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'sadiki-nyuad-2016') {
-      res.send(req.query['hub.challenge']);
-    }
-    res.send('Error, wrong token')
+  if (req.query['hub.verify_token'] === 'sadiki-nyuad-2016') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong token')
 });
 
 app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-          text = event.message.text
-          sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
+  messaging_events = req.body.entry[0].messaging
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
+    if (event.message && event.message.text) {
+      text = event.message.text
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
     }
-    res.sendStatus(200)
+  }
+  res.sendStatus(200)
 });
 
 // Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'));
 });
