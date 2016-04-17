780ae5f33dc2d4b1bf646aed5b7c4539b5d9633a
Harini Kannan <hkannan@mit.edu>
2016-04-17 10:30:52 +0400

    can now answer general questions

--- analyze-text.js
+++ analyze-text.js
@@ -1,9 +1,18 @@
 var _ = require('underscore');
+var tokenizer = require('./tokenizer');
 
 module.exports = function analyzeText(text) {
   var topics = _.map(text.split(' '), function(word) {
     return word.toLowerCase().replace('?', '');
   });
+
+  console.log("BEFORE");
+  console.log(topics);
+  console.log("\n");
+  topics = topics.concat(tokenizer(text));
+  console.log("AFTER");
+  console.log(topics)
+  console.log("\n");
 
   if (text.indexOf("salam") > -1) {
     topics.push("salam");
