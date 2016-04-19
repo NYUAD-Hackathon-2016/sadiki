8cb239027b3f3535a9668c872bd63104dce97ef5
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2016-04-19 22:43:17 +0400

    Change project information and modify seed script.

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,60 +5,80 @@ require('../config/database')(app);
 require('../models/setup');
 
 var Question = mongoose.model('Question');
+var Volunteer = mongoose.model('Volunteer');
 
-new Question({
+Volunteer.create({
+  'name': 'Gonzalo Beviglia',
+  'address': 'Fake St 123',
+  'phone': '+541156789012',
+  'country': 'Argentina',
+  'city': 'Capital Federal',
+  'topic': [
+    'health',
+    'emergency',
+    'language',
+    'legal',
+    'immigration',
+    'shelter',
+    'job',
+    'foot',
+    'culture'
+  ]
+});
+
+Question.create({
   locale: 'en',
   question: 'noanswer',
   topic: ['noanswer'],
   answer: 'Sorry, I can\'t help you with that. If you need further help, you can get in touch with: '
-}).save();
+});
 
-new Question({
+Question.create({
   locale: 'en',
   question: 'can i sign my kids into school?',
   topic: ['school', 'education', 'kids', 'kid'],
   answer: 'yes but they must speak turkish'
-}).save();
+});
 
-new Question({
+Question.create({
   locale: 'en',
   question: 'where can i teach them turkish?',
   topic: ['language', 'turkish'],
   answer: 'Bahceshir university recently launched a program to teach 300000 syrian students how to speak turkish.'
-}).save();
+});
 
-new Question({
+Question.create({
   locale: 'en',
   question: 'Where can i buy makdoos',
   topic: ['makdoos'],
   answer: 'Yousuf street is full of syrian restaurants OR call RAMA at 07678765. She makes makdoos every friday.'
-}).save();
+});
 
 
-new Question({
+Question.create({
   locale: 'ar',
   question: 'noanswer',
   topic: ['noanswer'],
   answer: 'Sorry, I can\'t help you with that. If you need further help, you can get in touch with: '
-}).save();
+});
 
-new Question({
+Question.create({
   locale: 'ar',
   question: 'فيني سجل اولادي بالمدرسة؟',
   topic: ['بالمدرسة', 'اولادي'],
   answer: 'اي فيك , بس لازم يحكو تركي'
-}).save();
+});
 
-new Question({
+Question.create({
   locale: 'ar',
   question: 'وين فيني علم ولادي تركي؟',
   topic: ['التركية', 'تركي'],
   answer: 'جامعة Bahcesehir University أطلقت برنامج تعليمي ل تعليم 300,000 طالب سوري اللغة التركية , فيك تروح وتسأل ,وهدا العنوان Çırağan Caddesi, Osmanpaşa Mektebi Sokak No: 4 - 6, 34349 Beşiktaş/İstanbul, Turkey الهاتف : +90 444 2 864'
-}).save();
+});
 
-new Question({
+Question.create({
   locale: 'ar',
   question: 'وين فيني اشتري قطرميز مكدوس',
   topic: ['مكدوس'],
   answer: 'في فوق ال 10 مطاعم سورية في شارع يوسف باشا بتقدر تروح هنيك , أو ! "راما" بتعمل مكدوس كل جمعة , هاي الرقم 05356789'
-}).save();
+});
